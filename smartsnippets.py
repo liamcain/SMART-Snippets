47d3778de696d1a8250f5c64ebdfa357f9319e28
Liam Cain <contactliam529@gmail.com>
2012-03-25 22:31:25 -0400

    code cleanup

--- smartsnippets.py
+++ smartsnippets.py
@@ -32,7 +32,7 @@ class NextSmartTabstopCommand(sublime_plugin.TextCommand):
         self.view.sel().add(next)
 
 class RunSmartSnippetCommand(sublime_plugin.TextCommand):
-    tabstop_words = []  # temporarily holds the words to be highlighted for tabstops
+    final_snip = ''
 
     reps = [
             ('insert'                  ,'self.insert'),
@@ -67,8 +67,7 @@ class RunSmartSnippetCommand(sublime_plugin.TextCommand):
     def parse_snippet(self,contents):
         new_contents = self.replace_all(contents, self.reps)
         tabstops = re.finditer('(\$\{)([0-9]+)(:)([a-zA-Z0-9 \"\']+)(\})', contents)
-        new_contents = re.split('(`)([a-zA-Z\s\'\"\(\).]+)(`)', new_contents)
-
+        new_contents = re.split('(`)([a-zA-Z0-9\s\'\"\(\)\[\].]+)(`)', new_contents)
         # my execution loop: alternates between exec & adding to snippet :)
         code = new_contents[0] == '`'
         if code: start = 1
@@ -82,7 +81,8 @@ class RunSmartSnippetCommand(sublime_plugin.TextCommand):
             code = not code
         return tabstops
     
-    def snippet_contents(self, trigger):
+    def snippet_contents(self):
+        trigger = self.get_trigger()
         package_dir = sublime.packages_path() + "/SMART_Snippets/"
         snip_file = package_dir + trigger + ".smart_snippet"
         with open(snip_file, 'r') as f:
@@ -98,16 +98,15 @@ class RunSmartSnippetCommand(sublime_plugin.TextCommand):
         return self.view.word(sel.a)
 
     def run(self, edit):
-        view = self.view
-        sel = view.sel()[0]
-        start_pos = sel.a - len(view.substr(view.word(sel)))
-        self.final_snip = ''
-        reg = self.get_trigger_reg()
-        trig = self.get_trigger()
-        snippet = self.snippet_contents(trig)
-        tabstops = self.parse_snippet(snippet)
+        view      = self.view
+        sel       = view.sel()[0]
+        start_pos = view.word(sel).begin()
+        reg       = self.get_trigger_reg()
+        snippet   = self.snippet_contents(trig)
+        tabstops  = self.parse_snippet(snippet)
+
         view.replace(edit, reg, self.final_snip)
-        self.generate_tabstops(edit,start_pos, tabstops)
-        self.tabstop_words = []
+        self.generate_tabstops(edit, start_pos, tabstops)
+
         if view.get_regions('smart_snippets'):
             self.view.run_command("next_smart_tabstop")
